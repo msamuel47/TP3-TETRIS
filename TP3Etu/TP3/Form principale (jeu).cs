dbd3a62ec20293de6d076b30cf0a88fc9bfbdb6f
félix bergeron <felixbergeron0147@hotmail.com>
2016-11-29 19:59:42 +0100

    fef

--- TP3Etu/TP3/Form principale (jeu).cs
+++ TP3Etu/TP3/Form principale (jeu).cs
@@ -120,7 +120,7 @@ namespace TP3
         /// <param name="e"></param>
         private void musiqueDambiacneToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            mediaPlayer.URL = "Wakfu AMV - Are U Ready _ Goultard Le Barbare Tribute _.mp3";
+            mediaPlayer.URL = "/Wakfu AMV - Are U Ready _ Goultard Le Barbare Tribute _.mp3";
             
             mediaPlayer.controls.play();
         }
